f0a0a9c15fdf54ef6b3bd4e39655cdaa7f9b168a
Jeremy Flood <J.flood592@gmail.com>
2016-02-25 16:12:36 -0700

    being a fuckwad because I dont understand git

--- index.js
+++ index.js
@@ -9,13 +9,6 @@ app.get('/goodbye', function (req, res) {
   res.send('Hello Goodbye')
 })
 
-<<<<<<< HEAD
 app.use(express.static(.));
-=======
-// app.use(express.static('landing.html'));
-// app.use(express.static('stylesheet.css'));
-
-app.use(express.static('.'))
->>>>>>> 45ee82b90b002fae7459bc1eb07e93ed303f5ea6
 
 app.listen(3000)
